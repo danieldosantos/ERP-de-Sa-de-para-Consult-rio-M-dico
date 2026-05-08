34a7b4c6e6e0b6b20abd656d8f500d26c9fe2b15
danieldosantos <85966300+danieldosantos@users.noreply.github.com>
2026-05-08 10:04:16 -0300

    Fix Prescricao table mapping for Eloquent

--- clinicerp/app/Models/Prescricao.php
+++ clinicerp/app/Models/Prescricao.php
@@ -1,5 +1,15 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-class Prescricao extends Model { use HasFactory; protected $fillable=['agendamento_id','medico_id','medicamentos']; }
+
+class Prescricao extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prescricoes';
+
+    protected $fillable = ['agendamento_id', 'medico_id', 'medicamentos'];
+}
